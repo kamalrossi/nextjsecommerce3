8f636d972087208ecae5676e6ee9326a5c7a61c1
Rossi Kamal <rossikamal@gmail.com>
2022-07-19 00:34:20 +0600

    Update Trophy.js

--- javascript-version/src/views/dashboard/Trophy.js
+++ javascript-version/src/views/dashboard/Trophy.js
@@ -5,7 +5,6 @@ import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 import { styled, useTheme } from '@mui/material/styles'
 
-// Styled component for the triangle shaped background image
 const TriangleImg = styled('img')({
   right: 0,
   bottom: 0,
@@ -13,7 +12,6 @@ const TriangleImg = styled('img')({
   position: 'absolute'
 })
 
-// Styled component for the trophy image
 const TrophyImg = styled('img')({
   right: 36,
   bottom: 20,
@@ -22,22 +20,21 @@ const TrophyImg = styled('img')({
 })
 
 const Trophy = () => {
-  // ** Hook
   const theme = useTheme()
   const imageSrc = theme.palette.mode === 'light' ? 'triangle-light.png' : 'triangle-dark.png'
 
   return (
     <Card sx={{ position: 'relative' }}>
       <CardContent>
-        <Typography variant='h6'>Congratulations John! 🥳</Typography>
+        <Typography variant='h6'>Congratulations Katerina Gorov 🥳</Typography>
         <Typography variant='body2' sx={{ letterSpacing: '0.25px' }}>
-          Best seller of the month
+          Author of the Month
         </Typography>
         <Typography variant='h5' sx={{ my: 4, color: 'primary.main' }}>
-          $42.8k
+          Total View 43,000
         </Typography>
         <Button size='small' variant='contained'>
-          View Sales
+          Watch View Statistics 
         </Button>
         <TriangleImg alt='triangle background' src={`/images/misc/${imageSrc}`} />
         <TrophyImg alt='trophy' src='/images/misc/trophy.png' />
