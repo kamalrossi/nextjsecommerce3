05b49140f7d056e637890955ff6d2c6cb2553e21
Rossi Kamal <rossikamal@gmail.com>
2022-07-19 06:52:00 +0600

    Update themeConfig.js

--- javascript-version/src/configs/themeConfig.js
+++ javascript-version/src/configs/themeConfig.js
@@ -1,20 +1,4 @@
-const themeConfig = {
-  // ** Layout Configs
-  templateName: 'Materio' /* App Name */,
-  mode: 'light' /* light | dark */,
-  contentWidth: 'boxed' /* full | boxed */,
-  // ** Routing Configs
-  routingLoader: true /* true | false */,
-  // ** Navigation (Menu) Configs
-  menuTextTruncate: true /* true | false */,
-  navigationSize: 260 /* Number in PX(Pixels) /*! Note: This is for Vertical navigation menu only */,
-  // ** Other Configs
-  responsiveFontSizes: true /* true | false */,
-  disableRipple: false /* true | false */
-}
-
-export default themeConfig
-const themeConfig = {
+ const themeConfig = {
   // ** Layout Configs
   templateName: 'MediaTrail' /* App Name */,
   mode: 'light' /* light | dark */,
@@ -29,4 +13,3 @@ const themeConfig = {
   disableRipple: false /* true | false */
 }
 
-export default themeConfig
